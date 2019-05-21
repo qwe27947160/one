2dd2e93d77d18dc694875eaf1249babba09c73ca
zf <398207902@qq.com>
2019-05-22 01:53:20 +0800

    321

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -26,6 +26,7 @@ class Ni
     	$results = (new Comic)  -> order('rand()') -> limit(12) ->select();
         $L = array();
     	foreach ($results as $data){
+            $data = $data -> getData();
             print_r($data);
             break;
             array_push($L,array("cd" => $data["ComicChapter"], "cn" => $data["urlname"],"cv" => $data["cover"] ,"ti" => $data["title"]));
