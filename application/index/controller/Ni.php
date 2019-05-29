600a8d07e2b3b39f6ec15b3f94d2713633f01a5b
zf <398207902@qq.com>
2019-05-29 22:47:29 +0800

    321

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -20,7 +20,8 @@ class Ni
         } else {
             $result = Db::table('comic')  -> order('rand()') -> limit(6) ->select();
             foreach ($result as $data) {
-                $data = $data -> getData();
+               
+                print_r($data);
                 $comicH5 .= '<li class=comicItem><a href=' . $data['urlname'] . 'class="comicLink title=' . $data['title'] . '><div class=_item-pic _item-lazy data-echo=' . $data['cover'] . 'style=background-image: url(' . $data['cover'] . ');><div class=video-duration>HD1280高清国语中字版</div></div><div class=video-con><h2 class=video-con-tit ellipsis-1>'. $data['title'] . '</h2></div></a></li>';
             }
             $view->assign(['comicH5' => $comicH5]);
