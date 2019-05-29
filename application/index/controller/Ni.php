ce55526915cd973d39e5af579b1b47c0a6d45745
zf <398207902@qq.com>
2019-05-29 22:46:28 +0800

    321

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -18,7 +18,7 @@ class Ni
         if($matches[1] == 'dm.') {
             echo $view->fetch('comic/index');
         } else {
-            $results = Db::table('comic')  -> order('rand()') -> limit(6) ->select();
+            $result = Db::table('comic')  -> order('rand()') -> limit(6) ->select();
             foreach ($result as $data) {
                 $data = $data -> getData();
                 $comicH5 .= '<li class=comicItem><a href=' . $data['urlname'] . 'class="comicLink title=' . $data['title'] . '><div class=_item-pic _item-lazy data-echo=' . $data['cover'] . 'style=background-image: url(' . $data['cover'] . ');><div class=video-duration>HD1280高清国语中字版</div></div><div class=video-con><h2 class=video-con-tit ellipsis-1>'. $data['title'] . '</h2></div></a></li>';
