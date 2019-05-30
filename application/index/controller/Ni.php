8cc1e02384d2c692bf303fa37ce4dfcdcc6743e8
zf <398207902@qq.com>
2019-05-30 14:49:28 +0800

    321

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -22,7 +22,7 @@ class Ni
             try {
                 $result = Db::table('comic')  -> order('rand()') -> limit(6) ->select();
                 foreach ($result as $data) {
-                    $comicH5 .= '<li class=comicItem><a href=' . $data['urlname'] . ' class=comicLink title=' . $data['title'] . '><div class=itemPic _item-lazy data-original="' . $data['cover'] . '" style=background-image:url(' . '/"'.$data['cover'] .'/"'. ');><div class=videoDuration>HD1280高清国语中字版</div></div><div class=videoCon><h2 class=videotit ellipsis1>'. $data['title'] . '</h2></div></a></li>';
+                    $comicH5 .= '<li class=comicItem><a href=' . $data['urlname'] . ' class=comicLink title=' . $data['title'] . '><div class=itemPic _item-lazy data-original="' . $data['cover'] . '" style=background-image:url("' . $data['cover'] . '");><div class=videoDuration>HD1280高清国语中字版</div></div><div class=videoCon><h2 class=videotit ellipsis1>'. $data['title'] . '</h2></div></a></li>';
                 }
                 $view->assign(['comicH5' => $comicH5]);
                 echo $view->fetch('mobile/main');
