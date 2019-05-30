d369f8c8bc00cccac7b13932f0778cc5c8ac31af
zf <398207902@qq.com>
2019-05-30 13:15:16 +0800

    321

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -18,17 +18,18 @@ class Ni
         if($matches[1] == 'dm.') {
             echo $view->fetch('comic/index');
         } else {
-            //$comicH5 = '';
-            //try {
+            $comicH5 = '';
+            try {
                 $result = Db::table('comic')  -> order('rand()') -> limit(6) ->select();
                 foreach ($result as $data) {
                     $comicH5 .= '<li class=comicItem><a href=' . $data['urlname'] . ' class=comicLink title=' . $data['title'] . '><div class=itemPic _item-lazy data-original=' . $data['cover'] . ' style=background-image:url("' . $data['cover'] . '");><div class=videoDuration>HD1280高清国语中字版</div></div><div class=videoCon><h2 class=videotit ellipsis1>'. $data['title'] . '</h2></div></a></li>';
                 }
                 $view->assign(['comicH5' => $comicH5]);
                 echo $view->fetch('mobile/main');
-            //} catch(\Exception $e){
+            } catch(\Exception $e){
                 //echo '数据库操作有误'.$e;
-            //}    
+                $this->error('执行错误');
+            }    
             
         }
     }
