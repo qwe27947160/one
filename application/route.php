cfa7984619fe73dba9b64152721bf8156df1e23d
zf <398207902@qq.com>
2019-06-02 20:19:39 +0800

    2

--- application/route.php
+++ application/route.php
@@ -40,4 +40,4 @@ Route::rule('/allvideo', 'index/LoadView/allvideo');
 
 /******************mobileview*********************/
 Route::rule('/mobile/comicChapter/:name', 'index/LoadView/comicChapter');
-Route::rule('/mobileComic/:chapterId/:pageId', 'index/LoadView/comicPage');
+Route::rule('/mobileComic/page/:chapterId/:pageId', 'index/LoadView/comicPage');
