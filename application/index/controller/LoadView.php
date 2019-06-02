42177d075d77d77cca3ae007e87834cb866969f8
zf <398207902@qq.com>
2019-06-02 11:35:30 +0800

    3

--- application/index/controller/LoadView.php
+++ application/index/controller/LoadView.php
@@ -54,6 +54,9 @@ class LoadView {
 	public function comicChapter($name) {
 		$imgPath = Db::table('comic') -> where('urlname', $name) -> find();
 		$lastChapter = Db::table('chapter') -> where('ComicChapter', $imgPath['ComicChapter']) -> field('ChapterName') -> order('ID desc') -> limit(1) -> find();
-		echo (new View()) -> fetch('mobile/comicChatper', ['imgPath' => "'".$imgPath['cover']."'", 'title' => $imgPath['title'], 'lastChapter' => $lastChapter['ChapterName']]);
+		$queryChapter = Db::table('chapter') -> where('ComicChapter', $imgPath['ComicChapter']) -> select();
+		var_dump($queryChapter);
+		//$comicChapterH5 .= "<li><a href="" target="_self" title="特别篇">特别篇</a></li>
+		//echo (new View()) -> fetch('mobile/comicChatper', ['imgPath' => "'".$imgPath['cover']."'", 'title' => $imgPath['title'], 'lastChapter' => $lastChapter['ChapterName']]);
 	}
 }
