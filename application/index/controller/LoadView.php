8d9736375c7bcf513d4169cd6cebd801b1b3bfe1
zf <398207902@qq.com>
2019-06-03 17:27:38 +0800

    1

--- application/index/controller/LoadView.php
+++ application/index/controller/LoadView.php
@@ -103,7 +103,7 @@ class LoadView {
 
 		    $comicH5 .= '<li class="comicItem"><a href="/mobile/comicChapter/' . $data['urlname'] . '" class="comicLink" title="' . $data['title'] . '"><div class="itemPic" data-original="' . $data['cover'] . '" style="background-image:url(\'' . $data['cover'] . '\');"><div class="videoDuration">更新到' . $lastChapter['ChapterName'] . '</div></div><div class="videoCon"><h2 class="videotit ellipsis1" style="text-align:center;">'. $data['title'] . '</h2></div></a></li>';
 		}
-		echo(new View()) -> fetch('mobile/allComic', ['H5' => $comicH5, 'name' => '全部在线漫画']);
+		echo(new View()) -> fetch('mobile/all', ['H5' => $comicH5, 'name' => '全部在线漫画']);
 	}
 
 	public function allMobileVideo() {
@@ -113,6 +113,6 @@ class LoadView {
             $lastChapter = Db::table('animationsdir') -> where('cvdirid', $data['ID']) -> field('dirname') -> order('dirbluesid desc') -> limit(1) -> find();
             $videoH5 .= '<li class="comicItem"><a href="/mobile' . $data['src'] . '" class="comicLink" title="' . $data['title'] . '"><div class="itemPic" data-original="' . $data['cover'] . '" style="background-image:url(\'' . $data['cover'] . '\');"><div class="videoDuration">更新到' . $lastChapter['dirname'] . '</div></div><div class="videoCon"><h2 class="videotit ellipsis1" style="text-align:center;">'. $data['title'] . '</h2></div></a></li>';
         }
-        echo(new View()) -> fetch('mobile/allComic', ['H5' => $videoH5, 'name' => '全部在线动画']);
+        echo(new View()) -> fetch('mobile/all', ['H5' => $videoH5, 'name' => '全部在线动画']);
 	}
 }
