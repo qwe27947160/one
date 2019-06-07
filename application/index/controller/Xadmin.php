405892edcd072ec465f721f8b237cec9a5ec11ac
zf <398207902@qq.com>
2019-06-08 01:38:55 +0800

    1

--- application/index/controller/Xadmin.php
+++ application/index/controller/Xadmin.php
@@ -12,6 +12,10 @@ class Xadmin extends Controller{
 		$pass = input('post.pass');
 		if(!Db::table('user_msg') -> where('username', $user) -> find()) {
 			echo json_encode(array('rs' => '帐号错误'));
+		} else if (!Db::table('user_msg') -> where('password', md5($pass)) -> find()) {
+			echo json_encode(array('rs' => '密码错误'));
+		} else {
+			echo json_encode(array('rs' => 'OK'));
 		}
 	}
 }
