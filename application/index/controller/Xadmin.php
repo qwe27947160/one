f8fea6b7e30b03b35ebd0fcc0ae8eb446361f1a1
zf <398207902@qq.com>
2019-06-08 20:18:19 +0800

    3

--- application/index/controller/Xadmin.php
+++ application/index/controller/Xadmin.php
@@ -30,7 +30,7 @@ class Xadmin extends Controller{
 			echo json_encode(array('rs' => '密码错误'));
 		} else {
 			Session::set('time', time() . 3600);
-			$this->redirect('/user/admin','200');
+			$this->success('登录成功','/user/admin');
 		}
 	}
 
