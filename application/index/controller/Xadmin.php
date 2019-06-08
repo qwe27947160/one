8fe93db638f91c3b333f3fb0a777c0375a84ebc3
zf <398207902@qq.com>
2019-06-09 02:05:42 +0800

    2

--- application/index/controller/Xadmin.php
+++ application/index/controller/Xadmin.php
@@ -26,16 +26,21 @@ class Xadmin extends Controller{
 		$user = input('post.user');
 		$pass = input('post.pass');
 		$userQyery = User_msg::get_user($user);
-		//print_r($user);
+		$request = Request::instance();
+		$userMsg = array('user' => $user, 'ip' => $request->ip(), 'ua' => $request->header('user-agent'), 'time' => time());
+		$loginMsg = new Loginmsg($userMsg);
 		if(!$userQyery) {
+			array_push($userMsg, 'state' =>'帐号错误');
+			$loginMsg -> save($userMsg);
 			echo json_encode(array('code' => '0', 'rs' => '帐号错误'));
 		} else if ($userQyery->password != md5($pass)) {
+			array_push($userMsg, 'state' =>'密码错误');
+			$loginMsg -> save($userMsg);
 			echo json_encode(array('code' => '0', 'rs' => '密码错误'));
 		} else {
 			//写入登录人信息
-			$request = Request::instance();
-			$loginMsg = new Loginmsg(['user' => $user, 'ip' => $request->ip(), 'ua' => $request->header('user-agent'), 'time' => time()]);
-			$loginMsg -> save();
+			array_push($userMsg, 'state' =>'登录成功');
+			$loginMsg -> save($userMsg);
 			//返回前端 让前端重定义url
 			Session::set('time', time() + 900);
 			echo json_encode(array('code' => '1', 'rs' => '登录成功'));
