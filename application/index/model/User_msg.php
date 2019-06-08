cfb9c23b1d27bb9eb195a157fb6d352e51f3bd06
zf <398207902@qq.com>
2019-06-08 12:44:57 +0800

    3

--- application/index/model/User_msg.php
+++ application/index/model/User_msg.php
@@ -5,10 +5,10 @@ use think\Model;
 
 class User_msg extends Model {
 	public static function Query($map) {
-		return self::where($map) -> field('username, password') -> find();
+		return static::where($map) -> field('username, password') -> find();
 	}
 
 	public static function get_user($username) {
-		return $this -> where(['username' => $user]) -> find();
+		return self::where(['username' => $user]) -> find();
 	}
 }
