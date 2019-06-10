44828cbc3c42e3d3a995be95b1e43ccf3120af83
zf <398207902@qq.com>
2019-06-10 21:53:51 +0800

    1

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -27,6 +27,7 @@ class Ni extends Controller
     }
 
     public function index() {
+        echo Session::get('userName')
         //$this->first();
         $host = $_SERVER['HTTP_HOST'];
         preg_match('/(.*\.)?\w+\.\w+$/', $host, $matches);
