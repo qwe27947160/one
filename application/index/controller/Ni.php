a15ba57e5d9d8ef782398de565eb78c75809ca99
zf <398207902@qq.com>
2019-06-16 18:03:29 +0800

    1

--- application/index/controller/Ni.php
+++ application/index/controller/Ni.php
@@ -79,11 +79,16 @@ class Ni extends Controller
     public function query_chapter() {
         $Qychapter = isset($_POST['qy'])? $_POST['qy'] : '';
         //观看记录查询
+        
         if(Session::get('userName')) {
             $map1 = array('status' => 1, 'user_name' => Session::get('userName'), 'cover' => $Qychapter);
             $queryRecord = (new Watch_record) -> where($map1) -> order('id DESC') -> find();
-            var_dump($queryRecord); 
+            var_dump($queryRecord);
+            if(!$queryRecord){
+
+            }
         }
+     
         //获取所有章节
         $query_comic = (new Comic) -> where('urlname',$Qychapter) ->field('ComicChapter') ->find();
         $map['ComicChapter'] = $query_comic->getData('ComicChapter');
