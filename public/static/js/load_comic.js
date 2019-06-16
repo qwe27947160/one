32f1a3548e0fc8865c54a283ac1930ac9818809c
zf <398207902@qq.com>
2019-06-17 01:01:39 +0800

    1

--- public/static/js/load_comic.js
+++ public/static/js/load_comic.js
@@ -41,6 +41,7 @@ function load_chapter($name){
 			if (rsdata.hasOwnProperty('popStatus')) {
 				
 			}
+			console.log(rsdata.chapterData);
 			for(var x=0; x<rsdata.chapterData; x++){
 				var cp_na = rsdata[x].pn;
 				var a = $("<a href = /page/" + rsdata[x].id +"/"+ rsdata[x].pagenum + " target=_blank>"+ cp_na +"</a>");
