f8ef4fa7795129c59264db2f725840031d625bf7
zf <398207902@qq.com>
2019-06-17 16:28:43 +0800

    1

--- public/static/js/load_comic.js
+++ public/static/js/load_comic.js
@@ -19,6 +19,10 @@
 					li.append(p);
 					p.append(a2);
 					a2.append(span);
+					
+					var li = $('<li><a data-pjax href="' + rsdata[x].cn + ' class="pic"><img data-original="' + rsdata[x].cv + 'alt="' + 
+						rsdata[x].ti + '></a><p class="cover"><a data-pjax href="' + rsdata[x].cn ' class="pic2"><span>' + rsdata[x].ti + 
+						'</span></a></p></li>');
 					$("#ccover").append(li);
 				}
 				//懒加载
