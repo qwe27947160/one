dddf8e75705cb0fe7a5a4584850852555d10e848
zf <398207902@qq.com>
2019-06-17 19:59:27 +0800

    1

--- public/static/js/load_comic.js
+++ public/static/js/load_comic.js
@@ -109,7 +109,7 @@
 					li.append(a1);
 					li.append(div);
 					*/
-					var li = $('<li class="vdcvli"><a data-pjax="" href="' + rsdata[x].src +'" title="' + rsdata[x].ti + '" target="_self" class="vdcvimg"><img class="vdcvloading" data-original="' + rsdata[x].cv + '" alt="' + rsdata[x].ti + '" style="display: inline;"><span class="vdcvmask"><i class="glyphicon glyphicon-play-circle glyphiconL"></i></span></a><div class="vdcvinfo"><a data-pjax="" href="' + rsdata[x].src + '">' + rsdata[x].ti + '</a><p><span class="vdcvf1">' + rsdata[x].cotdir + '</span></p></div></li>');
+					var li = $('<li class="vdcvli"><a data-pjax="" href="' + rsdata[x].src +'" title="' + rsdata[x].ti + '" target="_self" class="vdcvimg"><img class="vdcvloading" data-original="' + rsdata[x].cv + '" alt="' + rsdata[x].ti + '" style="display: inline;"><span class="vdcvmask"><i class="glyphicon glyphicon-play-circle glyphiconL"></i></span></a><div class="vdcvinfo"><a data-pjax="" href="' + rsdata[x].src + '">' + rsdata[x].ti + '</a><p><span class="vdcvf1">更新至' + rsdata[x].cotdir + '</span></p></div></li>');
 					$("#vdcv").append(li).hover(function(){
 						$(this).children(".vdcvmask").css("opacity","1");
 					},function(){
