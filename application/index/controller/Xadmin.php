35efa1d6cd8d4ef2ce1788e10d1bcb8647e878e9
zf <398207902@qq.com>
2019-07-01 19:05:59 +0800

    1

--- application/index/controller/Xadmin.php
+++ application/index/controller/Xadmin.php
@@ -84,7 +84,7 @@ class Xadmin extends Controller{
 			$videoResult = Db::table('animationscover') -> where(['ID' => $data['cover']]) -> find();
 			$cotdir = Db::table('animationsdir')-> where('cvdirid', $videoResult["ID"]) -> count('cvdirid');
 
-			$videoH5 .= '<ul class=vdcv id=vdcv><li class=vdcvli><a data-pjax href=' . $videoResult["src"] . ' title=' . $videoResult["title"] . ' target=_self class=vdcvimg><img class=vdcvloading data-original=' .  $videoResult["cover"] . 'alt=' . $videoResult["title"] . 'style=display:inline src=' . $videoResult["cover"] . '><span class=vdcvmask style=opacity:0;><i class=glyphicon glyphicon-play-circle glyphiconL></i></span></a><div class=vdcvinfo><a data-pjax href=' . $videoResult["src"] . '>' . $videoResult["title"] . '</a><p><span class=vdcvf1>更新至' . $cotdir . '集</span></p></div></li></ul>';
+			$videoH5 .= '<ul class=vdcv id=vdcv><li class=vdcvli><a href=' . $videoResult["src"] . ' title=' . $videoResult["title"] . ' target=_blank class=vdcvimg><img class=vdcvloading data-original=' .  $videoResult["cover"] . 'alt=' . $videoResult["title"] . 'style=display:inline src=' . $videoResult["cover"] . '><span class=vdcvmask style=opacity:0;><i class=glyphicon glyphicon-play-circle glyphiconL></i></span></a><div class=vdcvinfo><a  href=' . $videoResult["src"] . ' target=_blank>' . $videoResult["title"] . '</a><p><span class=vdcvf1>更新至' . $cotdir . '集</span></p></div></li></ul>';
 		}
 		echo (new view) -> fetch('X-admin/videoRecord', ['videoH5' => $videoH5]);
 	}
