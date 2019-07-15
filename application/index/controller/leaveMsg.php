39dc5f114d09eed252b64f89df2c0c4c74363ed1
zf <398207902@qq.com>
2019-07-15 20:40:26 +0800

    1

---
+++ application/index/controller/leaveMsg.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\index\controller;
+use \think\Controller;
+use \think\Db;
+use \think\View;
+use \think\Request;
+use \think\Config;
+use \think\Session;
+
+class UserLeaveMsg extends Controller {
+
+	public function addVideoLeaveMessage() {
+		echo('arg1');
+	}
+}
