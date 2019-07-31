a4308019f74a18f29e2beff640b9d4faa82444e5
zf <398207902@qq.com>
2019-07-31 19:57:21 +0800

    3

--- application/index/model/Leave_msg.php
+++ application/index/model/Leave_msg.php
@@ -9,11 +9,11 @@ class Leave_msg extends Model{
 	* @access public 
 	* @param $arg1 留言type
 	* @param $arg2 留言index 
-	* @param $limitStart, $limitend 排序
+	* @param $limitStart, $limitEnd 排序
 	* @return array 返回类型
 	*/ 
-	public function queryLeaveMessage($arg1, $arg2, $limitStart, $limitend) {
-		$data[] = $this -> where(['type' => $arg1, 'index' => $arg2]) -> field('userName, msg, time') -> limit($limitStart, $limitend) -> select();
+	public function queryLeaveMessage($arg1, $arg2, $limitStart, $limitEnd) {
+		$data[] = $this -> where(['type' => $arg1, 'index' => $arg2]) -> field('userName, msg, time') -> limit($limitStart, $limitEnd) -> select();
 		$data[] = $this -> where(['type' => $arg1, 'index' => $arg2]) -> count();
 		return $data;
 	}
